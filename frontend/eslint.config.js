9a6604ef2ba191bca036f68e2a0ab18ea8bb8ee8
Jeremías <jeremias.j.riv@gmail.com>
2026-07-27 23:10:30 -0300

    chore: update lint stack for security

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import { reactRefresh } from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -15,10 +15,11 @@ export default tseslint.config(
     },
     plugins: {
       "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      "react-refresh": reactRefresh.plugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
       "no-console": "error",
       "react-refresh/only-export-components": [
         "warn",
